25bc0cffef805ae0d916c45b3de3e840989211c5
Anar <AMurtuzov@RIB.int>
2018-11-01 16:34:13 +0300

    fixes

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import About_me from '../components/About-me.vue'
-import Projects from '../components/Projects.vue'
-import Contacts from '../components/Contacts.vue'
 
 Vue.use(VueRouter);
 
